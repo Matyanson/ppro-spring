b6c2d055bcef7748a9a10e7b96b77f059e774a49
Matyanson <peremma1@uhk.cz>
2024-10-02 15:20:32 +0200

    fix: added controller annotation

--- src/main/java/com/example/auta/controller/CarController.java
+++ src/main/java/com/example/auta/controller/CarController.java
@@ -1,6 +1,7 @@
 package com.example.auta.controller;
 
 import com.example.auta.model.Car;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import java.util.ArrayList;
 import java.util.List;
 
+@Controller
 public class CarController {
     List<Car> cars = new ArrayList<>();
 
