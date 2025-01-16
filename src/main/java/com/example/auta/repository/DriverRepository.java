8f63d98bab9b9a5d3152ac850b993a2343e9816b
Matyanson <matyas.perem@seznam.cz>
2025-01-16 19:34:59 +0100

    refactor: get rid of any car and driver implementations

--- src/main/java/com/example/auta/repository/DriverRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.auta.repository;
-
-import com.example.auta.model.Driver;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DriverRepository extends JpaRepository<Driver, Long> {
-
-}
